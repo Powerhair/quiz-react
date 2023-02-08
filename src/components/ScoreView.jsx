cacb8d948151a63bc891d35301523ca061b444a8
LubovPopova <gohre94@gmail.com>
2023-02-08 20:28:20 +0800

    1 commit

--- src/components/ScoreView.jsx
+++ src/components/ScoreView.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './ScoreView.css';
-import image from '../images/image.jpg';
+import image from '../images/result3.png';
 const ScoreView = ({ handleReset, score, questions }) => {
   return (
     <div>
-      <p>
-        Ваш результат{' '}
+      <p>Ваш результат: </p>
+      <div className="imagecontainer">
         {[...Array(score)].map((e, i) => (
           <img src={image} className="image" alt="score" key={i}></img>
-        ))}{' '}
-        из {questions.length}
-      </p>
+        ))}
+      </div>
+
+      <p>из {questions.length}</p>
       <button onClick={handleReset}>Заново</button>
     </div>
   );
